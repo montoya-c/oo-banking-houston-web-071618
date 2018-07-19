a593612c017ace115da03ee08b414deaee65a98f
github username <github email address>
2018-07-19 16:14:39 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,54 @@
 class BankAccount
+#one instance of the class can transfer money to another instance through a Transfer class.
+attr_reader :name, :status
+attr_writer :balance
+
+  def initialize (name)
+    @balance = 1000
+    @status = "open"
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def balance
+    @balance
+  end
+
+  def balance=(money)
+      @balance = money
+    end
+
+def status
+  @status
+end
+
+def status=(new_value)
+  @status = new_value
+end
+
+def deposit (num)
+  @balance += num
+  @balance
+end
+
+def display_balance
+  "Your balance is $#{self.balance}."
+end
+
+def valid?
+ if self.status == "open" && self.balance > 0
+   true
+ else
+   false
+end
+end
+
+def close_account
+@status = "closed"
+return self.status
+end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,52 @@
 class Transfer
-  # your code here
+  # Transfers start out in a "pending" status. They can be executed and go to a "complete" state. They can also go to a "rejected" status. A completed transfer can also be reversed and go into a "reversed" status.
+attr_reader :sender, :receiver, :amount
+attr_accessor :status
+
+def initialize (sender, receiver, status = "pending", amount)
+  @sender = sender
+  @receiver = receiver
+  @status = status
+  @amount = amount
+end
+
+def sender
+  @sender
+end
+
+def receiver
+  @receiver
+end
+
+def status
+  @status
+end
+
+def amount
+@amount
+end
+
+def valid?
+  sender.valid? == true && receiver.valid? == true && sender.balance - self.amount > 0
+end
+
+def execute_transaction
+  if self.valid? && status != "complete"
+  self.receiver.balance += self.amount
+  self.sender.balance -= self.amount
+  self.status = "complete"
+else
+  self.status = "rejected"
+  "Transaction rejected. Please check your account balance."
+end
+end
+
+def reverse_transfer
+ if self.status == "complete"
+   self.receiver.balance -= self.amount
+   self.sender.balance += self.amount
+   self.status = "reversed"
+end
+end
+
 end
